0984395a0707dd24f9837df8ccef0821a1e94ff0
momobln <kashshaazam@gmail.com>
2025-08-25 18:16:52 +0200

    DOne

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-// IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+
+const app = express();
+
+//  Middleware 
+app.use(logger('dev'));                                      
+app.use(express.static(path.join(__dirname, 'public')));     
+app.use(express.json());                                     
 
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-// CREATE EXPRESS APP
-// Here you should create your Express app:
+//  Routes 
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'home-page.html'));
+});
 
+app.get('/cat', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'cat-page.html'));
+});
 
+app.get('/data', (req, res) => {
+  const user = { name: 'John Doe', age: 30, at: req.requestTime };
+  res.json(user); 
+});
 
-// MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
+app.get('/test', (req, res) => {
+  res.send(`requestTime from middleware: ${req.requestTime}`);
+});
 
+//  404 
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
+//  500 
+app.use((err, req, res, next) => {
+  console.error(' Server Error:', err);
+  res.status(500).send('Internal Server Error');
+});
 
-// ROUTES
-// Start defining your routes here:
-
-
-
-// START THE SERVER
-// Make your Express server listen on port 5005:
+//  Start 
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000! 🚀');
+});
